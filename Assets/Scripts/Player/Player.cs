594bdd7c525aec10e5b66178a1db0300f2d55d15
Crazykelpi <dries.geysens@gmail.com>
2024-08-17 13:33:34 +0200

    Change growing function to be linear, camera zooms out when growing

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,7 +10,10 @@ public class Player : MonoBehaviour
     [Header("Grow Variables")]
     [Range(0, 1f)]
     public float growMultiplier = 0.05f;
-
+    public float totalSize = 1f;
+    [Range(1, 10f)]
+    public float maxTotalSize = 1f;
+    public bool isGrowing = false;
 
 
     // Dash variables
@@ -31,8 +34,15 @@ public class Player : MonoBehaviour
     [Range(1, 10)]
     public float speedMultiplier = 1f;
 
+    [Header("Camera Variables")]
     [SerializeField]
     Transform cameraTransform;
+    [SerializeField]
+    Camera mainCamera;
+    [Range(0, 0.01f)]
+    public float cameraZoomOutFactor = 0.01f;
+    [Range(5f,20f)]
+    public float maxCameraSize = 20f;
 
     InputReader input;
     // Start is called before the first frame update
@@ -42,7 +52,6 @@ public class Player : MonoBehaviour
     }
     private void Start()
     {
-
         dashCoolDown = maxDashCoolDown;
     }
     // Update is called once per frame
@@ -79,6 +88,10 @@ public class Player : MonoBehaviour
         {
             Grow();
         }
+        else
+        {
+            isGrowing = false;
+        }
         
         MoveCamera();
     }
@@ -98,7 +111,11 @@ public class Player : MonoBehaviour
 
     void MoveCamera()
     {
+        // If player is growing zoom out, otherwise keep the same distance.
+        mainCamera.orthographicSize = isGrowing ? Mathf.Min(mainCamera.orthographicSize + transform.localScale.x * cameraZoomOutFactor, maxCameraSize) : mainCamera.orthographicSize;
+        //mainCamera.orthographicSize += transform.localScale.x * cameraZoomOutFactor;
         cameraTransform.position = new Vector3(transform.position.x, transform.position.y, cameraTransform.position.z);
+        
     }
     void Dash(float time)
     {
@@ -119,8 +136,12 @@ public class Player : MonoBehaviour
     }
     void Grow()
     {
-        
-        transform.localScale *= Time.deltaTime * growMultiplier + 1;
+
+        totalSize += Time.deltaTime * growMultiplier;
+        totalSize = Mathf.Min(totalSize, maxTotalSize);
+        transform.localScale = new Vector3(totalSize, totalSize, 0);
         speedMultiplier /= Time.deltaTime * growMultiplier + 1;
+        isGrowing = true;
+        
     }
 }
